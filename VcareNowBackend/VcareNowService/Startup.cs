053d253ddf3a032a525ce89e12b98e2718052fe0
Mohammed Ameen <38343547+ameen435@users.noreply.github.com>
2019-09-15 23:28:38 +0200

    Adding CORS
    Adding CORS

--- VcareNowBackend/VcareNowService/Startup.cs
+++ VcareNowBackend/VcareNowService/Startup.cs
@@ -50,6 +50,7 @@ namespace VcareNowService
             }
 
             // app.UseHttpsRedirection();
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseMvc();
         }
     }
