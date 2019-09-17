2190fecd6caac59a00832783ec134ff1951eb399
Jomon T Jose <jose.jomon@live.com>
2019-09-17 23:00:18 +0530

    fixed issue

--- VcareNowClient/src/app/app.module.ts
+++ VcareNowClient/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { PatientComponent } from '.c:/HealthApp/VcareNowClient/src/patient/patient.component';
+import { PatientComponent } from './patient/patient.component';
 
 @NgModule({
    declarations: [
